19361df5c1ca66bdcd9b73b2df0c29cbbffddb95
henriquef96 <a.henriquefarias96@gmail.com>
2025-11-24 19:33:32 -0300

    first commit

---
+++ my-links/src/App.tsx
@@ -0,0 +1,24 @@
+import { createBrowserRouter } from "react-router"
+import { Home } from './pages/home'
+import { Admin } from './pages/admin'
+import { Login } from './pages/login'
+import { Networks } from './pages/networks'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
+  }, {
+    path: '/login',
+    element: <Login />
+  },
+  {
+    path: '/admin',
+    element: <Admin />
+  }, {
+    path: '/networks',
+    element: <Networks />
+  }
+])
+
+export { router };
