d5cb5835b9354d12440e6edd652b1672500abc5d
henriquef96 <a.henriquefarias96@gmail.com>
2025-11-24 19:36:01 -0300

    update

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,7 @@
+export function Home() {
+    return (
+        <div className="flex flex-col w-full py-4 item-center justify-center">
+            <h1 className="text-neutral-200 ">Home</h1>
+        </div>
+    )
+}
